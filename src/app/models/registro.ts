20738345c8cf6d29c75c04842c590d7ca3febef9
israel1947 <israeldadidsdvsf@gmail.com>
2022-05-31 09:49:28 -0500

    Implemented scan button operation

---
+++ src/app/models/registro.ts
@@ -0,0 +1,36 @@
+export class Register{
+    public format:string
+    public text:string
+    public type:string
+    public icon:string
+    public createDate:Date
+
+    constructor(format:string,text:string){
+        this.format=format;
+        this.text=text;
+
+        this.createDate = new Date();
+        this.determineType();
+    }
+
+    private determineType(){
+      const startText = this.text.substring(0,4);
+      console.log('Type: '+startText);
+
+      switch (startText) {
+        case 'http':
+          this.type = 'http';
+          this.icon = 'globe';
+          break;
+        case 'geo:':
+          this.text = 'geo';
+          this.icon = 'pin';
+          break;
+      
+        default:
+          this.type = 'Unrecognized';
+          this.icon =  'create';
+      }
+      
+    }
+}
